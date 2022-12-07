e12b0797ca191ae425affcc31072040591edad72
Willy Andersen <jowander_@hotmail.com>
2022-12-07 17:38:11 +0100

    register html added

--- src/openLoginModal.mjs
+++
@@ -1,9 +0,0 @@
-"use strict"
-const loginBtn = document.querySelector("#login-btn");
-const loginModal = document.querySelector("#login-modal");
-console.log(loginModal);
-
-
-loginBtn.addEventListener("click", () => {
-  
-})
