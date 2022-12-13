0c718d9d8c2b909709254af7d08e223092783bfa
Willy Andersen <jowander_@hotmail.com>
2022-12-13 15:51:21 +0100

    show more btn working

--- src/js/listings/getListings.mjs
+++ src/js/listings/getListings.mjs
@@ -17,12 +17,21 @@ const user = localStorage.getItem("userName");
 const itemCard = document.querySelector("#item-card");
 const profileAvatar = document.querySelector("#profile-avatar");
 
-const json = await apiCallGetListings(`${baseUrl}/listings`, optionGet);
+const json = await apiCallGetListings(
+    `${baseUrl}/listings?limit=30&_active=true`,
+    optionGet
+);
+console.log(json);
 itemCard.innerHTML = "";
 for (let i = 0; i < json.length; i++) {
     const jsonData = json[i];
     if (i <= 9) {
-        createMainItemListing(itemCard, jsonData);
+        createMainItemListing(
+            itemCard,
+            jsonData.media,
+            jsonData.title,
+            jsonData.id
+        );
     }
 }
 
@@ -31,7 +40,17 @@ const jsonProfile = await apiCallProfiles(
     optionGetAuth
 );
 console.log(jsonProfile);
-document
-    .querySelector("#show-more")
-    .addEventListener("click", createMainItemListing);
+document.querySelector("#show-more").addEventListener("click", () => {
+    for (let i = 10; i < json.length; i++) {
+        const jsonData = json[i];
+        if (i <= 19) {
+            createMainItemListing(
+                itemCard,
+                jsonData.media,
+                jsonData.title,
+                jsonData.id
+            );
+        }
+    }
+});
 createProfileAvatar(profileAvatar, jsonProfile);
